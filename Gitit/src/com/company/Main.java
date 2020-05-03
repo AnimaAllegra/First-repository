61df67e0c8274fecac1606a45c034537e5ce14d0
Taras Shchesniak <tarassesnak@icloud.com>
2020-05-03 19:31:10 +0300

    ..commit

--- Gitit/src/com/company/Main.java
+++ Gitit/src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Fitst Commit");
+        int [] sac = new int[10];
+        for (int i = 0; i <sac.length; i++){
+            System.out.println("sss");
+        }
+
     }
 }
