1054bb4c0c1c0456777efa2bfab32daa082ae6f3
Taras Shchesniak <tarassesnak@icloud.com>
2020-05-15 16:55:27 +0300

    nothing3

--- here tasks 1/zd3.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-//Создайте программу, выводящую на экран все неотрицательные элементы последовательности 90 85 80 75 70 65 60 ….
-public class zd3 {
-    public static void main(String[] args) {
-        for(int a = 95; a>=0; a-=5){
-            System.out.print(a + " ");
-        }
-    }
-}
